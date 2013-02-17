efcd57ae060f8c8808fb51386d7901cd8ea801cb
Dr. ERDI Gergo <gergo@erdi.hu>
2013-02-17 20:29:28 +0800

    More examples from the blogpost

--- blog.lisp
+++ blog.lisp
@@ -20,11 +20,36 @@
              (defvar nats (cons 0 (map + 1) nats))
              (defvar main nats))))
     (in-fresh-context
-     (let ((g (parse-program *prog*)))
+     (let ((g (parse-program p])))
        (simplify-apps g)
        (with-open-file (s "graph-02.dot" :direction :output :if-exists :supersede)
          (dot-from-graph g s))
        (reduce-to-whnf (nth 1 (gnode-args (gderef g))))
        (with-open-file (s "graph-02-tail.dot" :direction :output :if-exists :supersede)
+         (dot-from-graph g s))
+       g))))
+
+(defun blog-3 ()
+  (let ((p `(,@+lib-list+
+             (defvar main
+               (let ((zig (cons 0 zag))
+                     (zag (cons 1 zig)))
+                 zig)))))
+    (in-fresh-context
+     (let ((g (parse-program p)))
+       (simplify-apps g)
+       (with-open-file (s "graph-03.dot" :direction :output :if-exists :supersede)
+         (dot-from-graph g s))
+       g))))
+
+(defun blog-4 ()
+  (let ((p `(,@+lib-list+
+             (defvar main
+               (let ((xs xs))
+                 (cons 1 xs))))))
+    (in-fresh-context
+     (let ((g (parse-program p)))
+       (simplify-apps g)
+       (with-open-file (s "graph-03-bottom.dot" :direction :output :if-exists :supersede)
          (dot-from-graph g s))
        g))))
